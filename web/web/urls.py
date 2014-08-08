b699dbd4676fa2e0f20491afce0a7b72af5edfc1
hif <hif@simnet.is>
2014-08-08 18:14:29 +0000

    Commander for web

--- web/web/urls.py
+++ web/web/urls.py
@@ -8,12 +8,11 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'web.views.home', name='home'),
     # url(r'^web/', include('web.foo.urls')),
+    url(r'^$', include('twistedbrew.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^$', include('twistedbrew.urls')),
 )

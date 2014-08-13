fa71135d59b0d6ea42e90ccd1db8f2bc2e98966b
hif <hif@simnet.is>
2014-08-13 20:12:14 +0000

    sqlite3 support

--- web/settings.py
+++ web/settings.py
@@ -11,6 +11,15 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'database/twistedbrew.db',               # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        #'USER': 'admin',
+        #'PASSWORD': 'admin',
+        #'HOST': '127.0.0.1',             # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        #'PORT': '5432',                      # Set to empty string for default.
+    },
+    'postgres': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'twistedbrew',               # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:

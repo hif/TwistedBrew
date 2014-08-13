52449e13c1e7ce1c43db385d4a7e15ee9eb43b12
hif <hif@simnet.is>
2014-08-13 00:43:32 +0000

    Django structure revamped and simplified.

---
+++ web/urls.py
@@ -0,0 +1,25 @@
+from django.conf.urls import patterns, include, url
+import views
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'web_x.views.home', name='home'),
+    # url(r'^web_x/', include('web_x.foo.urls')),
+    # url(r'^$', include('twistedbrew.urls')),
+    url(r'^$', views.home, name='home'),
+    url(r'^index[/]$', views.home, name='home'),
+    url(r'^home[/]$', views.home, name='home'),
+    url(r'^brews[/]$', views.brews, name='brews'),
+    url(r'^workers[/]$', views.workers, name='workers'),
+    url(r'^commands[/]$', views.commands, name='commands'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
---
+++ web/views.py
@@ -0,0 +1,80 @@
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+# imports from within the Django web_x app must be scoped below the web_x package
+from models import Brew, Worker, Command
+from forms import CommanderForm
+from masters.brewcommander import BrewCommander
+
+def home(request):
+    context = RequestContext(request)
+    worker_list = Worker.objects.order_by('-type')
+    brew_list = Brew.objects.order_by('-name')
+    lastmessage = 'None'
+
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CommanderForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            lastmessage = form.cleaned_data['command']
+            commander = BrewCommander()
+            command, params = commander.parse_command(lastmessage)
+            commander.sendmaster(command, params)
+            #return HttpResponseRedirect('/')
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = CommanderForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+
+    context_dict = {
+        'workers': worker_list,
+        'brews': brew_list,
+        'form':form,
+        'lastmessage':lastmessage,
+    }
+
+    return render_to_response('home.html', context_dict, context)
+
+def brews(request):
+    context = RequestContext(request)
+    brew_list = Brew.objects.order_by('-name')
+
+    context_dict = {
+        'brews': brew_list,
+    }
+
+    return render_to_response('brews.html', context_dict, context)
+
+
+def workers(request):
+    context = RequestContext(request)
+    worker_list = Worker.objects.order_by('-name')
+
+    context_dict = {
+        'workers': worker_list,
+    }
+
+    return render_to_response('workers.html', context_dict, context)
+
+
+def commands(request):
+    context = RequestContext(request)
+    command_list = Command.objects.order_by('-name')
+
+    context_dict = {
+        'commands': command_list,
+    }
+
+    return render_to_response('commands.html', context_dict, context)

66e589e2d7ee03c3f81c0c0a8634ea009cbd3b29
hif <hif@simnet.is>
2014-09-16 20:52:55 +0000

    Messages live feed

--- web/admin.py
+++ web/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 # imports from within the Django web_x app must be scoped below the web_x package
-from models import Worker, Measurement, Command
+from models import Worker, Measurement, Command, Message
 
 admin.site.register(Worker)
 admin.site.register(Measurement)
 admin.site.register(Command)
+admin.site.register(Message)
--- web/views.py
+++ web/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+from django.utils import timezone
+from django.db.models import Min
 from django.views.generic import ListView
 from django.core.context_processors import csrf
 from django.template import RequestContext
@@ -9,6 +11,7 @@ from brew.models import Brew
 from forms import *
 from masters.brewcommander import BrewCommander
 import json, random
+from datetime import timedelta as timedelta
 import utils.logging as log
 from utils.dateutils import *
 
@@ -106,11 +109,41 @@ def messages_clear(request):
     return HttpResponseRedirect('/messages')
 
 
+def message_head(request):
+    return HttpResponse('<tr><th>Timestamp</th><th>Type</th><th>Message</th></tr>')
+
+
+def message_rows(request, latest_timestamp, max_rows):
+    rows_html = list()
+    counter = 0
+    limit = int(max_rows)
+    if latest_timestamp == u'0':
+        obj = Message.objects.all().aggregate(Min('timestamp'))
+        if obj['timestamp__min'] is None:
+            converted_timestamp = timezone.now()
+        else:
+            converted_timestamp = obj['timestamp__min'] - timedelta(seconds=1)
+    else:
+        converted_timestamp = datetime.fromtimestamp(float(latest_timestamp)/1000.0)
+        converted_timestamp = timezone.get_current_timezone().localize(converted_timestamp)
+    rows = Message.objects.filter(timestamp__gt=converted_timestamp)\
+        .order_by('-timestamp')
+    for row in rows:
+        str_timestamp = '{0}:{1}:{2}'.format(
+            unicode(row.timestamp.hour).zfill(2),
+            unicode(row.timestamp.minute).zfill(2),
+            unicode(row.timestamp.second).zfill(2))
+        rows_html.append('<tr><td>{0}</td><td>{1}</td><td>{2}</td></tr>'.format(str_timestamp, row.type, row.text))
+        counter += 1
+        if counter >= limit:
+            break;
+    return HttpResponse(json.dumps(rows_html), content_type="application/json")
+
 
 def charts(request):
     context = RequestContext(request)
 
-    chart_time = json.dumps(['00:00','00:05','00:10','00:15'])
+    chart_time = json.dumps(['00:00', '00:05', '00:10', '00:15'])
     chart_data = [0, 10, 15, 20]
     chart_set = [65, 65, 65, 65]
 
@@ -131,9 +164,11 @@ def charts_update(request):
     if request.POST:
         last_timestamp_ms = request.POST.getlist('timestamp')
         worker = request.POST.get('worker')
-    log.debug(last_timestamp_ms)
-    last_timestamp = ms_to_datetime(int(last_timestamp_ms[0]))
-    log.debug(last_timestamp)
+    log.debug('charts log: {0}'.format(last_timestamp_ms))
+    #last_timestamp = ms_to_datetime(int(last_timestamp_ms[0]))
+    last_timestamp = datetime.fromtimestamp(float(last_timestamp_ms[0])/1000.0)
+    last_timestamp = timezone.get_current_timezone().localize(last_timestamp)
+    log.debug('charts log: {0}'.format(last_timestamp))
 
     #latest_measurement_set = Measurement.objects.filter(device__iexact='Temperature').filter(timestamp__gt=last_timestamp)
     latest_measurement_set = Measurement.objects.filter(worker=worker).\
